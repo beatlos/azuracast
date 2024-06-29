afdd1368b9f50ffb7fb2f1396db9338c920de2ec
beatlos <70963118+beatlos@users.noreply.github.com>
2024-06-30 00:27:35 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,15 +7,3 @@ terraform {
   }
 }
 
-# Set the variable value in *.tfvars file
-# or using the -var="hcloud_token=..." CLI option
-variable "hcloud_token" {
-  description = "Token for Hetzner Cloud"
-  type        = string
-  sensitive   = true
-}
-
-# Configure the Hetzner Cloud Provider
-provider "hcloud" {
-  token = var.hcloud_token
-}
