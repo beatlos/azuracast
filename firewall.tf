de4425e9f1a983bae9f213ecd6e713dbc289a17d
beatlos <70963118+beatlos@users.noreply.github.com>
2024-06-30 00:43:45 +0200

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -4,11 +4,21 @@ resource "hcloud_firewall" "web" {
   rule {
     direction = "in"
     protocol  = "tcp"
-    port      = [ "80", "443" ]
+    port      = "80"
     source_ips = [
       "0.0.0.0/0",
       "::/0"
     ]
   }
-  
+
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
 }
