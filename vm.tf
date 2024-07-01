ecb9e8080cf977e2b5c9251756464ca2373d4366
beatlos <70963118+beatlos@users.noreply.github.com>
2024-07-01 17:55:21 +0200

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,3 +1,24 @@
+resource "hcloud_server" "server" {
+  name        = "azurecast"
+  server_type = "cpx11"
+  image       = "rocky-9"
+  location    = "nbg1"
+  ssh_keys    = "hcloud_ssh_key.default.id"
+
+  network {
+    network_id = hcloud_network.network.id
+    ip         = "10.0.1.5"
+    alias_ips  = [
+      "10.0.1.6",
+      "10.0.1.7"
+    ]
+  }
+
+  depends_on = [
+    hcloud_network_subnet.network-subnet
+  ]
+}
+
 # Create a new SSH key
 resource "hcloud_ssh_key" "default" {
   name       = "automation"
