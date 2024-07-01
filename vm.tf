d3fc941f27df69a057b86f4bf2dcc9f6e8f6118b
beatlos <70963118+beatlos@users.noreply.github.com>
2024-07-01 17:59:31 +0200

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -8,10 +8,6 @@ resource "hcloud_server" "server" {
   network {
     network_id = hcloud_network.network.id
     ip         = "10.0.1.5"
-    alias_ips  = [
-      "10.0.1.6",
-      "10.0.1.7"
-    ]
   }
 
   depends_on = [
